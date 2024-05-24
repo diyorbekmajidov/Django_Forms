04f5d5a6241ad3e45a492821bccb443c1bf8a3d3
Diyorbek <majidovdiyorbek741@gmail.com>
2024-05-24 16:58:04 +0500

    send messages to email and reset password

--- user/urls.py
+++ user/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('login/', views.custom_login, name='login'),
     path('logout/', views.custom_logout, name='logout'),
     path('profile/<str:username>/', views.profile, name='profile'),
-    path('activate/<uidb64>/<token>', views.activate, name='activate')
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    path('password_change/', views.password_change, name='password_change'),
+    path('password_reset', views.password_reset_request, name="password_reset"),
+    path('reset/<uidb64>/<token>', views.passwordResetConfirm, name="password_reset_confirm")
     # path("login/", auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     # path("logout/", auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 ]
